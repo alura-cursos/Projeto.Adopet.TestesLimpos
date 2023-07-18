14f3fe73ffb82f172a0574abb76c60e857c71fcf
bessax <andrebessax@gmail.com>
2023-07-18 11:42:39 -0300

    #video 2.3 - Teste de unidade comando import.

--- Alura.Adopet.Testes/ImportTest.cs
+++ Alura.Adopet.Testes/ImportTest.cs
@@ -38,5 +38,28 @@ namespace Alura.Adopet.Testes
             var listaPet = await httpClientPet.ListPetsAsync();
             Assert.NotEmpty(listaPet);
         }
+
+
+        [Fact]
+        public async Task QuandoListaVaziaNuncaDeveChamarCreatePetAsync()
+        {
+            //Arrange
+            var leitorDeArquivo = new Mock<LeitorDeArquivo>(MockBehavior.Default,
+                It.IsAny<string>());
+            var listaDePet = new List<Pet>();
+            leitorDeArquivo.Setup(_ => _.RealizaLeitura()).Returns(listaDePet);
+
+            //var httpClient = new AdopetAPIClientFactory().CreateClient("API");
+            var httpClientPet = new Mock<HttpClientPet>(MockBehavior.Default,
+                It.IsAny<HttpClient>());
+                       
+            var import = new Import(httpClientPet.Object, leitorDeArquivo.Object);
+            string[] args = { "import", "lista.csv" };
+            //Act
+            await import.ExecutarAsync(args);
+
+            //Assert
+            httpClientPet.Verify(_ => _.CreatePetAsync(It.IsAny<Pet>()), Times.Never);
+        }
     }
 }
