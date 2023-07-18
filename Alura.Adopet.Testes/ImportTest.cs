33aa2d5287904aef2ace48bb4e44ecd7c7460d00
bessax <andrebessax@gmail.com>
2023-07-18 12:19:39 -0300

    #desafio: QuandoArquivoNaoExistenteDeveGerarException

--- Alura.Adopet.Testes/ImportTest.cs
+++ Alura.Adopet.Testes/ImportTest.cs
@@ -39,7 +39,6 @@ namespace Alura.Adopet.Testes
             Assert.NotEmpty(listaPet);
         }
 
-
         [Fact]
         public async Task QuandoListaVaziaNuncaDeveChamarCreatePetAsync()
         {
@@ -61,5 +60,55 @@ namespace Alura.Adopet.Testes
             //Assert
             httpClientPet.Verify(_ => _.CreatePetAsync(It.IsAny<Pet>()), Times.Never);
         }
+
+        [Fact]
+        public async Task QuandoArquivoNaoExistenteDeveGerarException()
+        {
+            //Arrange
+            var leitor = new Mock<LeitorDeArquivo>(MockBehavior.Strict, It.IsAny<string>());
+            var listaDePet = new List<Pet>();
+            leitor.Setup(_ => _.RealizaLeitura()).Throws<FileNotFoundException>();
+
+            var httpClientPet = new Mock<HttpClientPet>(MockBehavior.Default,
+                It.IsAny<HttpClient>());
+
+            string[] args = { "import", "lista.csv" };
+
+            var import = new Import(httpClientPet.Object, leitor.Object);
+
+            //Act+Assert
+            await Assert.ThrowsAnyAsync<Exception>(() => import.ExecutarAsync(args));
+        }
+
+        [Fact]
+        public async Task QuandoListaNaoVaziaDeveChamarCreatePetAsync()
+        {
+            //Arrange
+       
+            var listaDePet = new List<Pet>();
+            var pet = new Pet(new Guid("456b24f4-19e2-4423-845d-4a80e8854a99"),
+                  "Lima", TipoPet.Cachorro);
+            listaDePet.Add(pet);
+          
+            var leitor = new Mock<LeitorDeArquivo>(MockBehavior.Strict, It.IsAny<string>());
+         
+            var httpClientPet = new Mock<HttpClientPet>(MockBehavior.Default,
+                          It.IsAny<HttpClient>());
+                   
+            leitor.Setup(_ => _.RealizaLeitura()).Returns(listaDePet);
+         
+            string[] args = { "import", "lista.csv" };
+            var import = new Import(httpClientPet.Object,
+                leitor.Object);
+
+            //Act
+            await import.ExecutarAsync(args);
+
+            //Assert
+            httpClientPet.Verify(_ => _.CreatePetAsync(It.IsAny<Pet>()), Times.Once);
+
+
+        }
+
     }
 }
